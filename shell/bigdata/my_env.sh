589ac00727fd649d6bcf4097e22eb457cb07e213
pianozcl <pianozcl@gmail.com>
2021-08-03 22:39:09 +0800

    practice sql

---
+++ shell/bigdata/my_env.sh
@@ -0,0 +1,12 @@
+#JAVA_HOME
+export JAVA_HOME=/home/module/jdk1.8.0_212
+export PATH=$PATH:$JAVA_HOME/bin
+
+#HADOOP_HOME
+export HADOOP_HOME=/home/module/hadoop-3.1.3
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+
+#MY_BIN
+export MY_BIN=/home/mybin
+export PATH=$PATH:$MY_BIN
